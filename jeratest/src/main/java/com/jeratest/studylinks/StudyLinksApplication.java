78744a63874b1e4c9fb6a72558b481aeac52d4d3
Larissa Marães <larissa.maraes@gmail.com>
2018-06-27 21:19:55 -0400

    começando o projeto: criando classe StudyLink

---
+++ jeratest/src/main/java/com/jeratest/studylinks/StudyLinksApplication.java
@@ -0,0 +1,12 @@
+package com.jeratest.studylinks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyLinksApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudyLinksApplication.class, args);
+    }
+}
---
+++ jeratest/src/main/java/com/jeratest/studylinks/domain/StudyLink.java
@@ -0,0 +1,49 @@
+package com.jeratest.studylinks.domain;
+
+import javax.persistence.*;
+import java.util.List;
+import com.google.common.collect.Lists;
+
+@Entity
+public class StudyLink {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String title;
+    private String url;
+    @Column
+    @ElementCollection(targetClass=String.class)
+    private List<String> categories = Lists.newArrayList();
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public List<String> getCategories() {
+        return categories;
+    }
+
+    public void setCategories(List<String> categories) {
+        this.categories = categories;
+    }
+}
